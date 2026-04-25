2994b4586f84f62bb5e3fd286828c7f61f711192
Vivian Battista <vitianbatt@users.noreply.github.com>
2026-04-25 11:08:51 +0800

    Reduce coarse_count to 300 and start date to 2010 for faster backtesting

--- static/strategies/52-weeks-high-effect-in-stocks.py
+++ static/strategies/52-weeks-high-effect-in-stocks.py
@@ -11,6 +11,10 @@
 #
 # QC implementation changes:
 #   - Universe consists of 500 most liquid stocks traded on NYSE, AMEX, or NASDAQ.
+#
+# Personal notes:
+#   - Reduced coarse_count from 500 to 300 to speed up backtesting during experimentation.
+#   - Starting from 2010 to focus on more recent market behavior.
 
 from numpy import floor
 from AlgorithmImports import *
@@ -18,7 +22,7 @@ from AlgorithmImports import *
 class Weeks52HighEffectinStocks(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2000, 1, 1)
+        self.SetStartDate(2010, 1, 1)  # Changed from 2000 to 2010 for faster iteration
         self.SetCash(100000)
 
         self.SetSecurityInitializer(lambda x: x.SetMarketPrice(self.GetLastKnownPrice(x)))
@@ -34,7 +38,7 @@ class Weeks52HighEffectinStocks(QCAlgorithm):
         
         self.symbol = self.AddEquity('SPY', Resolution.Daily).Symbol
         
-        self.coarse_count = 500
+        self.coarse_count = 300  # Reduced from 500 to speed up backtesting
         self.selection_flag = False
         self.UniverseSettings.Resolution = Resolution.Daily
         self.AddUniverse(self.CoarseSelectionFunction, self.FineSelectionFunction)
@@ -67,138 +71,4 @@ class Weeks52HighEffectinStocks(QCAlgorithm):
                 continue
             
             self.data[symbol] = SymbolData(symbol, self.period)
-            history = self.History(symbol, self.period, Resolution.Daily)
-            if history.empty:
-                self.Log(f"Not enough data for {symbol} yet")
-                continue
-            closes = history.loc[symbol].close
-            for time, close in closes.iteritems():
-                self.data[symbol].update(close)
-                
-        return [x for x in selected if self.data[x].is_ready()]
-
-    def FineSelectionFunction(self, fine):
-        fine = [x for x in fine if x.MarketCap != 0 and \
-                ((x.SecurityReference.ExchangeId == "NYS") or (x.SecurityReference.ExchangeId == "NAS") or (x.SecurityReference.ExchangeId == "ASE"))]
-        
-        group = {}
-        for stock in fine:
-            symbol = stock.Symbol
-            
-            industry_group_code = stock.AssetClassification.MorningstarIndustryGroupCode
-            if industry_group_code == 0: continue
-            
-            # Adding stocks in groups.
-            if not industry_group_code in group:
-                group[industry_group_code] = []
-            
-            max_high = self.data[symbol].maximum()
-            price = self.data[symbol].get_latest_price()
-            
-            stock_prilag = (stock, price / max_high)
-            group[industry_group_code].append(stock_prilag)
-        
-        top_industries = []
-        low_industries = []
-        
-        if len(group) != 0: 
-            # Weighted average of ratios calc.
-            industry_prilag_weighted_avg = {}
-            for industry_code in group:
-                total_market_cap = sum([stock_prilag_data[0].MarketCap for stock_prilag_data in group[industry_code]])
-                if total_market_cap == 0: continue
-                industry_prilag_weighted_avg[industry_code] = sum([stock_prilag_data[1] * (stock_prilag_data[0].MarketCap / total_market_cap) for stock_prilag_data in group[industry_code]])
-            
-            if len(industry_prilag_weighted_avg) != 0:
-                # Weighted average industry sorting.
-                sorted_by_weighted_avg = sorted(industry_prilag_weighted_avg.items(), key=lambda x: x[1], reverse = True)
-                top_industries = [x[0] for x in sorted_by_weighted_avg[:6]]
-                low_industries = [x[0] for x in sorted_by_weighted_avg[-6:]]
-        
-        long = []
-        short = []
-        for industry_code in top_industries:
-            for stock_prilag_data in group[industry_code]:
-                symbol = stock_prilag_data[0].Symbol
-                long.append(symbol)
-        
-        for industry_code in low_industries:
-            for stock_prilag_data in group[industry_code]:
-                symbol = stock_prilag_data[0].Symbol
-                short.append(symbol)
-                
-        long_w = self.Portfolio.TotalPortfolioValue / self.holding_period / len(long)
-        short_w = self.Portfolio.TotalPortfolioValue / self.holding_period / len(short)
-        
-        # symbol/quantity collection
-        long_symbol_q = [(x, floor(long_w / self.data[x].get_latest_price())) for x in long]
-        short_symbol_q = [(x, -floor(short_w / self.data[x].get_latest_price())) for x in short]
-        
-        self.managed_queue.append(RebalanceQueueItem(long_symbol_q + short_symbol_q))
-        
-        return long + short
-
-    def OnData(self, data):
-        if not self.selection_flag:
-            return
-        self.selection_flag = False
-
-        remove_item = None
-        
-        # Rebalance portfolio
-        for item in self.managed_queue:
-            if item.holding_period == self.holding_period:
-                # Liquidate
-                for symbol, quantity in item.symbol_q:
-                    self.MarketOrder(symbol, -quantity)
-                remove_item = item
-            
-            # Trade execution    
-            if item.holding_period == 0:
-                open_symbol_q = []
-                
-                for symbol, quantity in item.symbol_q:
-                    if self.Securities.ContainsKey(symbol) and self.Securities[symbol].IsTradable:
-                        self.MarketOrder(symbol, quantity)
-                        open_symbol_q.append((symbol, quantity))
-                            
-                # Only opened orders will be closed        
-                item.symbol_q = open_symbol_q
-                
-            item.holding_period += 1
-            
-        # We need to remove closed part of portfolio after loop. Otherwise it will miss one item in self.managed_queue.
-        if remove_item:
-            self.managed_queue.remove(remove_item)
-
-    def Selection(self):
-        self.selection_flag = True
-
-class RebalanceQueueItem():
-    def __init__(self, symbol_q):
-        # symbol/quantity collections
-        self.symbol_q = symbol_q  
-        self.holding_period = 0
-
-class SymbolData():
-    def __init__(self, symbol, period):
-        self.Symbol = symbol
-        self.Price = RollingWindow[float](period)
-    
-    def update(self, value):
-        self.Price.Add(value)
-    
-    def is_ready(self):
-        return self.Price.IsReady
-     
-    def maximum(self):
-        return max([x for x in self.Price])
-        
-    def get_latest_price(self):
-        return [x for x in self.Price][0]
-
-# Custom fee model.
-class CustomFeeModel(FeeModel):
-    def GetOrderFee(self, parameters):
-        fee = parameters.Security.Price * parameters.Order.AbsoluteQuantity * 0.00005
-        return OrderFee(CashAmount(fee, "USD"))
+            his
